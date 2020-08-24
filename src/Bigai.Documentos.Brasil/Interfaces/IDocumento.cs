98a29730f0d6152c12f87ee13760df58e8134f14
Marcos Cruz <Marcos Cruz@MARCOS-CRUZ>
2020-08-24 17:57:05 -0300

    Adicionado uma Interface para representar um Documento

---
+++ src/Bigai.Documentos.Brasil/Interfaces/IDocumento.cs
@@ -0,0 +1,18 @@
+﻿namespace Bigai.Documentos.Brasil.Interfaces
+{
+    /// <summary>
+    /// <see cref="IDocumento"/> representa um contrato para manipulação de um documento.
+    /// </summary>
+    public interface IDocumento
+    {
+        /// <summary>
+        /// Representa o número de um documento.
+        /// </summary>
+        string NumeroDocumento { get; }
+
+        /// <summary>
+        /// Sigla do estado emissor composta por 2 letras, segundo a norma ISO-3166.
+        /// </summary>
+        string EstadoEmissor { get; }
+    }
+}
